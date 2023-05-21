1a3943bab99462812ef8427abf596458f40bf789
Kellan Stevens <kellan@thamessystems.com>
2023-05-21 19:12:13 +0200

    Added migrates for 2 new tables: dates and reviews

---
+++ database/migrations/2023_05_21_000000_create_dates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dates', function (Blueprint $table) {
+            $table->id('date_id');
+            $table->date('date');
+            $table->string('place_name');
+            $table->string('description')->nullable();
+            $table->string('website')->nullable();
+            $table->string('google_maps_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dates');
+    }
+}
---
+++ database/migrations/2023_05_21_000000_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id('review_id');
+            $table->unsignedBigInteger('date_id');
+            $table->string('reviewer_name');
+            $table->integer('rating');
+            $table->text('review')->nullable();
+            $table->foreign('date_id')->references('date_id')->on('dates')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
